4586d78c79b4f73201e922e85687fbf4de78c17d
Essakkiappan <119649278+Essakkiappan-Subash@users.noreply.github.com>
2023-03-13 20:48:45 +0530

    Add files via upload

---
+++ Full Page Scroll With JavaScript, Vertical Slider  Swiper/assets/js/index.js
@@ -0,0 +1,39 @@
+var swiper = new Swiper(".mySwiper", {
+    direction: 'vertical',
+    sliderPerView: 1,
+    spaceBetween: 0,
+    mousewheel: true,
+    pagination: {
+      el: ".swiper-pagination",
+      type: "progressbar",
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
+
+
+
+// var swiper = new Swiper('.swIper-container', {
+//     direction: 'vertical',
+//     sliderPerView: 1,
+//     spaceBetween: 0,
+//     mousewheel: true,
+//     pagination: {
+//         el:'.swiper-pagination',
+//         type: 'progressbar',
+//     },
+// });
+
+
+// var swiper = new Swiper('.swiper-container', {
+//     direction: 'vertical',
+//     sliderPerView: 1,
+//     spaceBetween: 0,
+//     mousewheel: true,
+//     pagination: {
+//         el:'.swiper-pagination',
+//         type: 'progressbar',
+//     }
+// })
